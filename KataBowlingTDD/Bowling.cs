5f56df2b8a93a033199f9ff0a341ee6c0d18d41a
AlexEnCode <alexgabrielline@gmail.com>
2024-01-26 14:31:38 +0100

    Orange : OnRecommenceAZero

--- KataBowlingTDD/Bowling.cs
+++ KataBowlingTDD/Bowling.cs
@@ -11,30 +11,16 @@ namespace KataBowlingTDD
     public class Bowling
     {
 
-        public int Tour { get; set; } = 10;
-        public int Lancer { get; set; } = 2;
-        public int Score{ get; set; }
-        public int Bonus { get; set; }
-        public int Quille { get; set; }
+        public int round { get; set; };
 
-        public bool Jouer()
-        {
-            for (int i =1; i < Tour; i++)
-            {
-             
-            }
-            return true;
-        }
+
+
 
         public void Round()
         {
-            throw new NotImplementedException(); 
 
+            
         }
 
-        public void ScorePartie()
-        { 
-            throw new NotImplementedException(); 
-        }
     }
 }
--- TestKataBowling/UnitTest1.cs
+++ TestKataBowling/UnitTest1.cs
@@ -4,28 +4,7 @@ namespace TestKataBowling
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void FinDeLaPartieAuTour10()
-        {
-            //arrange
-            var partieBowling = new Bowling();
-            //act
-            var result = partieBowling.Jouer();
-            //assert
-            Assert.IsTrue(result);
-
-        }
-        [TestMethod]
-        public void DeuxLancerParRound()
-        {
-            //arrange
-            var partieBowling = new Bowling();
-            //act
-            partieBowling.Round();
-            var Lancer = partieBowling.Round;
-            //assert
-            Assert.AreEqual(Lancer, 2);
-        }
+       
     }
 }
 
