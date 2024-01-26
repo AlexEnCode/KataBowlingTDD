2e3d8f6eba7d086cbdd8429f9b45cbc88af9d0e3
AlexEnCode <alexgabrielline@gmail.com>
2024-01-26 12:21:13 +0100

    features(class bowling) :Initial commit

---
+++ KataBowlingTDD/Bowling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace KataBowlingTDD
+{
+    internal class Bowling
+    {
+    
+        public int Tour { get; set; }
+        public int Lancer { get; set; }
+        public int Score{ get; set; }
+        public int Bonus { get; set; }
+        public int Quille { get; set; }
+
+        public void Jouer()
+        {
+         throw new NotImplementedException();   
+        }
+
+        public void ScorePartie()
+        { 
+            throw new NotImplementedException(); 
+        }
+    }
+}
